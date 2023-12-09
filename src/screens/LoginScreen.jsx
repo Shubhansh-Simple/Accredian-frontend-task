fe323a7a9da521eb0144aafa0e79fb93c9c6ccd9
shubhansh-simple <shubhansh7777@gmail.com>
2023-12-09 23:30:16 +0530

    Login and signup pages UI complete with material-ui

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -2,10 +2,76 @@
  * Login Page of our application
  */
 
+import React from 'react'
+import { 
+  Grid,
+  Paper,
+  Avatar,
+  TextField,
+  Button,
+  Typography,
+  Checkbox,
+  Link,
+  FormControlLabel
+} from '@mui/material'
+
+import LockIcon from '@mui/icons-material/Lock';
+
 const LoginScreen = () => {
-  return (
-    <h1>Login Page</h1>
-  );
+    const paperStyle = { padding :20,
+                         height:'70vh',
+                         width:280, 
+                         margin:'20px auto'
+    }
+    const avatarStyle = { backgroundColor:'#1bbd7e' }
+    const btnstyle = { margin:'8px 0' }
+
+    return(
+      <Grid>
+        <Paper elevation={10} style={paperStyle}>
+          <Grid align='center'>
+            <Avatar style={avatarStyle}>
+              <LockIcon />
+            </Avatar>
+            <h2>Sign In</h2>
+          </Grid>
+
+          {/* Username Field */}
+          <TextField label='Username' 
+                     placeholder='Enter username' 
+                     variant='outlined' fullWidth required />
+
+          {/* Password Field */}
+          <TextField label='Password' 
+                     placeholder='Enter password' 
+                     type='password' 
+                     variant='outlined' fullWidth required/>
+
+          <FormControlLabel
+              control={
+                <Checkbox name='checkedB' color='primary' />
+              }
+              label='Remember me' />
+
+          { /* Submit Button */ }
+          <Button type='submit' 
+                  color='success' 
+                  variant='contained' 
+          style={btnstyle} fullWidth>
+            Sign in
+          </Button>
+
+          { /* Signup Link */ }
+          <Typography >
+            New User ?   
+            <Link href='/signup' >
+                    Register
+            </Link>
+          </Typography>
+
+        </Paper>
+      </Grid>
+    );
 }
 
 export default LoginScreen;
--- src/screens/SignupScreen.jsx
+++ src/screens/SignupScreen.jsx
@@ -1,12 +1,82 @@
 /*
- * Sign up Page of our application
+ * Signup Page of our application
  */
 
+import React from 'react'
+import { 
+  Grid,
+  Paper,
+  Avatar,
+  TextField,
+  Button,
+  Typography,
+  Link,
+} from '@mui/material'
+
+import LockIcon from '@mui/icons-material/Lock';
+
 const SignupScreen = () => {
-  return (
-    <h1>Signup Page</h1>
-  );
+    const paperStyle = { padding :20,
+                         height:'70vh',
+                         width:280, 
+                         margin:'20px auto'
+    }
+    const avatarStyle = { backgroundColor:'#1bbd7e' }
+    const btnstyle = { margin:'8px 0' }
+
+    return(
+      <Grid>
+        <Paper elevation={10} style={paperStyle}>
+          <Grid align='center'>
+            <Avatar style={avatarStyle}>
+              <LockIcon />
+            </Avatar>
+            <h2>Sign Up</h2>
+          </Grid>
+
+          {/* Username Field */}
+          <TextField label='Username' 
+                     placeholder='Enter username' 
+                     variant='outlined' fullWidth required />
+
+          {/* Email Field */}
+          <TextField label='Email' 
+                     placeholder='Enter email' 
+                     type='email'
+                     variant='outlined' fullWidth required />
+
+          {/* Password Field */}
+          <TextField label='Password' 
+                     placeholder='Enter password' 
+                     type='password' 
+                     variant='outlined' fullWidth required/>
+
+          {/* Password Field Confirm */}
+          <TextField label='Confirm Password' 
+                     placeholder='Confirm password' 
+                     type='password' 
+                     variant='outlined' fullWidth required/>
+
+          <br />
+          { /* Submit Button */ }
+          <Button type='submit' 
+                  color='error' 
+                  variant='contained' 
+                  style={btnstyle} fullWidth>
+            Register
+          </Button>
+
+          { /* Signup Link */ }
+          <Typography >
+            Already have an account ? 
+            <Link href='/login' >
+              Login
+            </Link>
+          </Typography>
+
+        </Paper>
+      </Grid>
+    );
 }
 
 export default SignupScreen;
-
