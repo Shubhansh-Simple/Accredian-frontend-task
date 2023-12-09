5edeed037c79f29d46ee19ce76bad39cf7557572
shubhansh-simple <shubhansh7777@gmail.com>
2023-12-09 20:49:09 +0530

    Add routers to our application

--- src/App.js
+++ src/App.js
@@ -2,15 +2,27 @@
  * Base template of our application
  */
 
+// React-Router
+import { Outlet } from 'react-router-dom';
+
 // Local
 import Header from './components/Header';
 import Footer from './components/Footer';
 
+
+/*
+ * Router uses the outlet ( index.js )
+ * like which component need to show
+ * there
+ */
 const App = () => {
   return (
     <>
       <Header />
-        <h1>Welcome to app</h1>
+
+        {/* From router path */}
+        <Outlet />
+
       <Footer />
     </>
   );
--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
+/*
+ * It's the url mapping with component file
+ * like urls.py in django
+ */
+
 // React 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+// React Router
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider
+} from 'react-router-dom';
+
+// Css file
 import './index.css';
 
 // Local
@@ -9,10 +23,26 @@ import App from './App';
 
 import reportWebVitals from './reportWebVitals';
 
+/* 
+ * Consist all routes of our application
+ * Router - Mapping URL with Pages 
+ */
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+
+    // Parent is App.js
+    <Route path='/'
+           element={ <App /> }>
+    </Route>
+  )
+);
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
