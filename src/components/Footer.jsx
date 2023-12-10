75102c4a634710f9d63d61cd384b0ae5761854d0
shubhansh-simple <shubhansh7777@gmail.com>
2023-12-10 08:00:35 +0530

    Footer is complete ( Ready-to-deploy-mode )

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,11 @@
 /*
  * Footer Of Entire Application
  */
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
 
 const Footer = () => {
 
@@ -10,12 +15,42 @@ const Footer = () => {
 
   return (
     <footer>
-      <br />
-      <p className='text-center bg-dark text-light'>
-        <small className='text-secondary'>
-          &copy; {currentYear} Copyright:  
-          </small> <strong>{developerId}</strong>
-      </p>
+      <Paper sx={{  marginTop: 'calc(10% + 60px)',
+                    position: 'fixed',
+                    bottom: 0,
+                    width: '100%'
+             }} 
+             component="footer" 
+             square 
+             variant="outlined">
+        <Container maxWidth='lg'>
+
+          <Box
+            sx={{
+              flexGrow: 1,
+              justifyContent: 'center',
+              display: 'flex',
+            }}
+          >
+            <Typography variant='small' color='initial'>
+              <p>
+                &copy; {currentYear} Copyright :      
+                <strong>{developerId}</strong>
+              </p>
+            </Typography>
+          </Box>
+        </Container>
+      </Paper>
+
+
+
+
+
+
+
+
+
+
     </footer>
   );
 };
