315eb22d8ab63a05387cd2c034d94b4e6153fd23
shubhansh-simple <shubhansh7777@gmail.com>
2023-12-10 08:01:34 +0530

    Header UI completed, Left - (Add logic to Logout button left)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,44 @@
  * Header of the entire application
  */
 
+// React-router
+import { Link } from 'react-router-dom';
+
+// React-Material-UI
+import AppBar        from '@mui/material/AppBar';
+import Toolbar       from '@mui/material/Toolbar';
+import Typography    from '@mui/material/Typography';
+import Button        from '@mui/material/Button';
+
+// Material Icons
+import LogoutIcon    from '@mui/icons-material/Logout';
+
 const Header = () => {
   return (
     <header>
-      <h1>Accredian Assignment</h1>
+
+      <AppBar position='static' sx={{ bgcolor: "black" }}>
+        <Toolbar>
+          {/* The Typography component applies 
+               default font weights and sizes */}
+          <Typography
+              variant='h6'
+              component='div'
+              sx={{ flexGrow: 1 }}
+          >
+            <strong>Accredian</strong>
+          </Typography>
+
+          {/* Shows only if user is authenticated */}
+          <Button component={Link} 
+                  to="login" 
+                  variant="contained" 
+                  startIcon={<LogoutIcon />}
+                  color="error"> 
+            <b>Log out</b>
+          </Button>
+        </Toolbar>
+      </AppBar>
     </header>
   );
 }
