0323fd8ae54bccccf6232001e2d7b38a8a7153f8
shubhansh-simple <shubhansh7777@gmail.com>
2023-12-13 22:52:44 +0530

    Add sampleUsers for testing the frontend

---
+++ src/sampleUsers.js
@@ -0,0 +1,29 @@
+/*
+ * Three sample user in our local file
+ * We consider it as a user
+ */
+
+const users = [
+  {
+    'id' : 1,
+    'username' : 'shubhansh',
+    'email'    : 'shubhansh7777@gmail.com',
+    'password' : '123456',
+  },
+  {
+    'id' : 2,
+    'username' : 'himansh',
+    'email'    : 'himansh7777@gmail.com',
+    'password' : '123456',
+  },
+  {
+    'id' : 3,
+    'username' : 'shivansh',
+    'email'    : 'shivansh7777@gmail.com',
+    'password' : '123456',
+  }
+];
+
+export default users;
+
+
