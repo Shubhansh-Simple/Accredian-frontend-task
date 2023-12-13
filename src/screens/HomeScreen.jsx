6197b37b0b93a33ce45b52cb8cbae9e62b5fc864
shubhansh-simple <shubhansh7777@gmail.com>
2023-12-13 22:59:59 +0530

    Improve HomeScreen UI by adding the username visible

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -2,28 +2,31 @@
  * HomeScreen of our application
  */
 
-import { useEffect } from 'react';
-import { Grid } from '@mui/material'
+import { useEffect } from "react";
+import { Grid, Avatar } from "@mui/material";
+
+// Material-UI icons
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 
 /*
  * Sending the get request
  * with the token
  * if authenticated then stay
- * otherwise redirect to the 
+ * otherwise redirect to the
  * Login page
  * useEffect()
  */
 
 const HomeScreen = () => {
-
-  /* Send get request to the server 
-   * which decides weather i stay 
+  /* Send get request to the server
+   * which decides weather i stay
    * or redirected to login page
    */
-  useEffect( () => {
-    console.log('Use Effect is called.');
-  },[]); 
+  useEffect(() => {
+    console.log("Use Effect is called.");
+  }, []);
 
+  const avatarStyle = { backgroundColor:'#5cb85c' }
 
   return (
     <Grid
@@ -32,14 +35,22 @@ const HomeScreen = () => {
       direction="column"
       alignItems="center"
       justifyContent="center"
-      sx={{ minHeight: '90vh' }}
+      sx={{ minHeight: "90vh" }}
     >
-        <h2>Welcome To Home</h2>
-        <small style={{ color:'green' }}>( Login Successfully )</small>
+      <h2>Welcome To Home, Shubhansh</h2>
+
+      <Avatar style={avatarStyle}>
+        <CheckCircleIcon />
+      </Avatar>
+      <br />
+
+      <b>
+        <small style={{ color: "green" }}>
+          ( Login Successfully )
+        </small>
+      </b>
     </Grid>
   );
-}
+};
 
 export default HomeScreen;
-
-
