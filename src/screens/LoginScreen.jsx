670106e5065da58e30a8dbf77ce14170c34b1f66
shubhansh-simple <shubhansh7777@gmail.com>
2023-12-15 18:54:41 +0530

    UI of login & signup screen completed. ( Next - Add validation to form )

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -2,78 +2,99 @@
  * Login Page of our application
  */
 
-import React, {useState } from 'react';
-//import axios    from 'axios';
+// React
 
 // React-router
 import { Link } from 'react-router-dom';
 
 // React-Bootstrap
-import { Container, Card, Button, Row, Col } from 'react-bootstrap'; 
-
-// React-icons
+import { Form, 
+  Card,
+  Button,
+} from 'react-bootstrap'; 
 
 // Components
 import AuthRadioBtn from '../components/AuthRadioBtn';
 
 const LoginScreen = () => {
-
   /*
    * Fields Required for login
    * Username/Email and Password
    */
-  const [ username, setUsername ] = useState('');
-  const [ password, setPassword ] = useState('');
+  // const [ username, setUsername ] = useState('');
+  // const [ password, setPassword ] = useState('');
 
-  /* Detect Change In Username */
-  const onUsernameChange = (e) => {}
+  // /* Detect Change In Username */
+  // const onUsernameChange = (e) => {}
 
-  /* Detect Change In Password */
-  const onPasswordChange = (e) => {}
+  // /* Detect Change In Password */
+  // const onPasswordChange = (e) => {}
 
-  /* Submitting form data to backend */
-  const handleSubmit = e => {}
+  // /* Submitting form data to backend */
+  // const handleSubmit = e => {}
 
   /* JSX CODE */
   return(
-    <div className='text-center'>
-      <Container>
-        <AuthRadioBtn />
+    <div className='px-3 bg-light'>
+      <br />
+      <br />
 
-        {/* CARD */}
-        <Card className='my-2 py-5 rounded px-2'>
+      {/* CARD */}
+      <Card className='py-2 rounded bg-white'>
 
-          {/* CARD HEADER */}
+        {/* CARD HEADER */}
+        <Card.Header className='bg-white text-center'>
+          <AuthRadioBtn title={'Sign In'} iconStyle={'iconLogin'}/>
+        </Card.Header>
+
+        { /* CARD BODY */ }
+        <Card.Body className='bg-white'>
+
+          {/* Form */}
+          <Form>
+
+            {/* Username */}
+            <Form.Group className='mb-2 px-2 py-3' controlId='formBasicEmail'>
+              <Form.Label>
+                <strong>Username Or Email</strong>
+              </Form.Label>
+              <Form.Control size='lg' type='email' placeholder='name@example.com' />
+            </Form.Group>
+
+            {/* Password */}
+            <Form.Group className='mb-3 px-2 py-3' controlId='formBasicPassword'>
+              <Form.Label>
+                <strong>Password</strong>
+              </Form.Label>
+              <Form.Control size='lg' type='password' placeholder='Password' />
+            </Form.Group>
+          </Form>
+
+          {/* Submit Button */}
+          <br />
           <div className='text-center'>
-            <Card.Text as='h3'>
-              Header
-            </Card.Text>
-          </div>
-
-          { /* CARD BODY */ }
-          <Card.Body>
-            Body
-          </Card.Body>
-
-
-          { /* CARD FOOTER */ }
-          <Card.Footer className='bg-white'>
-            { /* Login button */ }
-            <br />
-            <Button variant='outline-primary' 
-                    size='md' 
+            <Button variant='primary w-50' 
+                    size='lg' 
                     onClick={{}}
                     className='px-5'>
               Sign In
             </Button>
-          </Card.Footer>
-        </Card>
-        <br />
-        <br />
-        <b>Not a member ? <span> </span>
-          <Link to='/signup'>Register</Link>
-        </b>
-      </Container>
+          </div>
+        </Card.Body>
+
+        <Card.Footer className='bg-white text-center'>
+          <br />
+          <br />
+          <div>
+            <b>Not a member ? <span> </span>
+              <Link to='/signup'>Register</Link>
+            </b>
+          </div>
+        </Card.Footer>
+      </Card>
+      <br />
+      <br />
+      <br />
     </div>
   );
 }
--- src/screens/SignupScreen.jsx
+++ src/screens/SignupScreen.jsx
@@ -1,103 +1,116 @@
 /*
- * Signup Page of our application
+ * Sign up Page of our application
  */
 
-import React, {useState } from 'react';
+// React
 
 // React-router
 import { Link } from 'react-router-dom';
 
-// React-bootstrap
-import { Container, Card, Button } from 'react-bootstrap';
+// React-Bootstrap
+import { Form, 
+  Card,
+  Button,
+} from 'react-bootstrap'; 
 
 // Components
 import AuthRadioBtn from '../components/AuthRadioBtn';
 
 const SignupScreen = () => {
-
   /*
-   * Following fields required for signup
-   * Username, Email, Password, Confirm Password
+   * Fields Required for login
+   * Username/Email and Password
    */
-  const [ username, setUsername ]               = useState('');
-  const [ password, setPassword ]               = useState('');
-  const [ email, setEmail ]                     = useState('');
-  const [ confirmPassword, setConfirmPassword ] = useState('');
+  // const [ username, setUsername ] = useState('');
+  // const [ password, setPassword ] = useState('');
 
+  // /* Detect Change In Username */
+  // const onUsernameChange = (e) => {}
 
-  /* Detect Change In Username */
-  const onUsernameChange = (e) => {
-    setUsername(e.target.value);
-  }
+  // /* Detect Change In Password */
+  // const onPasswordChange = (e) => {}
 
-  /* Detect Change In Email */
-  const onEmailChange = (e) => {
-    setEmail(e.target.value);
-  }
+  // /* Submitting form data to backend */
+  // const handleSubmit = e => {}
 
-  /* Detect Change In Password */
-  const onPasswordChange = (e) => {
-    setPassword(e.target.value);
-  }
-
-  /* Detect Change In Confirm Password */
-  const onConfirmPasswordChange = (e) => {
-    setConfirmPassword(e.target.value);
-  }
-
-  /* Submitting form data to backend */
-  const handleSubmit = e => {
-    e.preventDefault();
-    const formSubmitData = {
-      'username' : username,
-      'password' : password,
-      'email'    : email,
-      'confirmPassword' : confirmPassword
-    }
-    console.log('Data - ', formSubmitData);
-    console.log('JSON Data - ', JSON.stringify(formSubmitData) );
-  }
-
+  /* JSX CODE */
   return(
-    <div className='text-center'>
-      <Container>
-        <AuthRadioBtn />
 
-        {/* CARD */}
-        <Card className='my-2 py-5 rounded px-2'>
+    <div className='px-3 bg-light'>
+      <br />
+      <br />
 
-          {/* CARD HEADER */}
+      {/* CARD */}
+      <Card className='py-2 rounded bg-white'>
+
+        {/* CARD HEADER */}
+        <Card.Header className='bg-white text-center'>
+          <AuthRadioBtn title={'Sign Up'} iconStyle={'iconSignup'}/>
+        </Card.Header>
+
+        { /* CARD BODY */ }
+        <Card.Body className='bg-white'>
+
+          {/* Form */}
+          <Form>
+
+            {/* Username */}
+            <Form.Group className='mb-2 px-2 py-3' controlId='formBasicEmail'>
+              <Form.Label>
+                <strong>Username</strong>
+              </Form.Label>
+              <Form.Control size='lg' type='email' placeholder='Type your username' />
+            </Form.Group>
+
+            {/* Email */}
+            <Form.Group className='mb-2 px-2 py-3' controlId='formBasicEmail'>
+              <Form.Label>
+                <strong>Email address</strong>
+              </Form.Label>
+              <Form.Control size='lg' type='email' placeholder='name@example.com' />
+            </Form.Group>
+
+            {/* Password */}
+            <Form.Group className='mb-3 px-2 py-3' controlId='formBasicPassword'>
+              <Form.Label>
+                <strong>Password</strong>
+              </Form.Label>
+              <Form.Control size='lg' type='password' placeholder='Password' />
+            </Form.Group>
+
+            {/* Confirm Password */}
+            <Form.Group className='mb-3 px-2 py-3' controlId='formBasicPassword'>
+              <Form.Label>
+                <strong>Confirm Password</strong>
+              </Form.Label>
+              <Form.Control size='lg' type='password' placeholder='Retype-Password' />
+            </Form.Group>
+
+          </Form>
+
+          <br />
           <div className='text-center'>
-            <Card.Text as='h3'>
-              Header
-            </Card.Text>
-          </div>
-
-          { /* CARD BODY */ }
-          <Card.Body>
-            Body
-          </Card.Body>
-
-
-          { /* CARD FOOTER */ }
-          <Card.Footer className='bg-white'>
-            { /* Login button */ }
-            <br />
-            <Button variant='danger' 
-                    size='md' 
+            <Button variant='danger w-70' 
+                    size='lg' 
                     onClick={{}}
                     className='px-5'>
               Create Account
             </Button>
-          </Card.Footer>
+          </div>
+        </Card.Body>
 
-        </Card>
-        <br />
-        <br />
-        <b>Already a member ? <span> </span>
-          <Link to='/login'>Login</Link>
-        </b>
-      </Container>
+        <Card.Footer className='bg-white text-center'>
+          <br />
+          <br />
+          <div>
+            <b>Already a member ? <span> </span>
+              <Link to='/login'>Login</Link>
+            </b>
+          </div>
+        </Card.Footer>
+      </Card>
+      <br />
+      <br />
     </div>
   );
 }
