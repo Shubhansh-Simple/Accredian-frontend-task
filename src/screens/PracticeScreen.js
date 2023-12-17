a7b13760cf6ef1f7ff12e2d4dc86e1e4fd23e837
shubhansh-simple <shubhansh7777@gmail.com>
2023-12-18 02:25:46 +0530

    Practice react state with demo data before make changes into  signupform

--- src/screens/PracticeScreen.js
+++ src/screens/PracticeScreen.js
@@ -3,34 +3,74 @@
  */
 
 // Local
-import users from '../sampleUsers';
+// import users from '../sampleUsers';
+import { useState, useEffect } from 'react'; 
+
+import {Button} from 'react-bootstrap';
 
 const PracticeScreen = () => {
 
-  const ListUser = users.map( user => 
-    <tr>
-      <td>{user.username}</td>
-      <td>{user.email}</td>
-      <td>{user.password}</td>
-    </tr>
-  );
+  const messageList = [
+    {
+      'visibility': true,
+      'msg': 'Username must be atleast of 6 characters'
+    },
+    {
+      'visibility': true,
+      'msg': 'Username must not be more than 12 characters'
+    },
+    {
+      'visibility': true,
+      'msg': 'No spaces allowed'
+    },
+    {
+      'visibility': true,
+      'msg': 'Only these special characters allowed'
+    },
+    {
+      'visibility': true,
+      'msg': 'Username already exist'
+    },
+  ]
+
+  const [usernameErr, setUsernameErr] = useState(messageList);
+
+
+  const onButtonClick = ()=>{
+    console.log('Button Clicked');
+
+    let currentVal = [...usernameErr];
+    console.log("value of currentVal - ", currentVal);
+    currentVal[2].visibility=false;
+
+    setUsernameErr( currentVal );
+  }
+
+  useEffect(() => {
+    console.log('Value of usernameErr before - ', usernameErr);
+    setUsernameErr(messageList);
+  }, []);
 
   return (
-    <>
+    <div className='text-center'>
       {/* In order to run a logic in UI */}
       <h1 className='text-center text-danger'>Practice Page</h1>
       <br />
-      <table>
-        <tr>
-          <th>Username</th>
-          <th>Email</th>
-          <th>Password</th>
-        </tr>
-        {ListUser}
-      </table>
-    </>
+      <hr />
+      <h1 className='text-center text-info'>Checking value of messageList</h1>
+      <h1>{usernameErr.length}</h1>
+      {
+        usernameErr.map( (msg,index) =>{
+          if ( msg.visibility )
+            return <h4 key={index}>{index+1}.) {msg.msg}</h4>
+        })
+      }
+      <br />
+      <br />
+      <Button className='btn-success btn-lg' onClick={onButtonClick}>Change State</Button>
+      <hr />
+    </div>
   );
 };
 
 export default PracticeScreen;
-
