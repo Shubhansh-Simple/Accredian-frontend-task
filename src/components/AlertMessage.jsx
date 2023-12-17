21011a74bf1fa32ed2f7445f50e5c27143aaafb5
shubhansh-simple <shubhansh7777@gmail.com>
2023-12-18 02:22:17 +0530

    Update AlertMessage.jsx, Add icons for both success and err messages

--- src/components/AlertMessage.jsx
+++ src/components/AlertMessage.jsx
@@ -3,36 +3,57 @@
  * return - JSX
  */
 
+// React-icons
+import { TiTick }   from 'react-icons/ti';
+import { MdCancel } from "react-icons/md";
+
 /*
- * Helper function
- * Display the recieved msg
+ * Show success message with Tick icon
  */
-function ErrMessage( {msg} ){
+function SuccessMessage( {msg='Success'} ){
   return (
-    <div className='text-danger'>
-      <li>
-        <b>{msg}</b>
-      </li>
+    <div className='text-success'>
+      <TiTick />&nbsp;&nbsp;
+      <b>{msg}</b>
     </div>
   );
 }
 
 /*
- * Iterate the recieved msg list &
- * display them sequentially
+ * List <li> the error messages with icons and colors
  */
-function ErrMessageList( {msgList} ){
+function ErrMessageItem( {msg='Successfully Accepted'} ){
+  return (
+    <li>
+      <MdCancel />&nbsp;&nbsp;
+      <b>{msg}</b>
+    </li>
+  );
+}
+
+/*
+ * Iterate through entire message list
+ * but showing message based on message's visiblity 
+ */
+function ErrMessageList( {msgList=[]} ){
+  console.log('MsgList recieved - ', msgList, msgList.length );
   return (
     <ul className='text-danger'>
       {
-        msgList.map( (msg,index) => 
-          <ErrMessage key={index} msg={msg} />
-        )
+        msgList 
+          &&
+        msgList.map( (msg,index) => {
+          /*
+           * Show err message, only if message visibility is true 
+           */
+          if ( msg.visibility )
+            return <ErrMessageItem key={index} msg={msg.msg} />
+        })
       }
     </ul>
   );
 }
 
-export { ErrMessage, ErrMessageList };
+export { SuccessMessage, ErrMessageItem , ErrMessageList };
 
 
